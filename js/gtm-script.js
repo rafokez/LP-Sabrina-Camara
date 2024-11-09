cc437b8c7d067c2027673e946569497067b73c16
unknown <raphael.k.business@gmail.com>
2024-11-09 15:28:33 -0300

    commit terminado os wedding books

--- js/gtm-script.js
+++ js/gtm-script.js
@@ -1,4 +1,5 @@
-(function(w, d, s, l, i) {
+// Google Tag Manager Script
+(function (w, d, s, l, i) {
     w[l] = w[l] || [];
     w[l].push({
       'gtm.start': new Date().getTime(),
@@ -10,13 +11,12 @@
     j.async = true;
     j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
     f.parentNode.insertBefore(j, f);
-
-     // Adiciona um log no console para confirmar o carregamento do GTM
-     j.onload = function() {
-        console.log('Google Tag Manager carregado com sucesso.');
-      };
-      j.onerror = function() {
-        console.error('Erro ao carregar o Google Tag Manager.');
-      };
-
+  
+    // Adiciona um log no console para confirmar o carregamento do GTM
+    j.onload = function () {
+      console.log('Google Tag Manager carregado com sucesso.');
+    };
+    j.onerror = function () {
+      console.error('Erro ao carregar o Google Tag Manager.');
+    };
   })(window, document, 'script', 'dataLayer', 'GTM-WXJ2B6KM');
